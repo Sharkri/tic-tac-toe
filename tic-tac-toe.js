7db9616e459567817a0546edf4db0508b9ab95b5
Sharkri <sharkdude955@gmail.com>
2022-06-24 03:17:01 -0500

    added functionality to place markers

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,10 +1,39 @@
-const GAME_BOARD = {
-  gameBoard: ["x", "o", "x", "x", "o", "x", "x", "o", "x"],
-  render_gameBoard: function () {
-    const gameBoardHTML = document.querySelectorAll(".marker");
-    for (let divNum = 0; divNum < this.gameBoard.length; divNum++) {
-      gameBoardHTML[divNum].textContent = this.gameBoard[divNum];
+const game = (() => {
+  const board = {
+    gameBoard: ["", "", "", "", "", "", "", "", ""],
+  };
+
+  const marker = document.querySelectorAll(".marker");
+  const turn = document.querySelector(".turn");
+
+  function render() {
+    for (let divNum = 0; divNum < board.gameBoard.length; divNum++) {
+      marker[divNum].textContent = board.gameBoard[divNum];
     }
-  },
-};
-GAME_BOARD.render_gameBoard();
+  }
+
+  // Event Listener
+  marker.forEach((box) => {
+    box.addEventListener("click", () => {
+      if (box.textContent) return;
+
+      currentTurn = turn.textContent.includes("X") ? "X" : "O";
+      turn.textContent = `${currentTurn === "X" ? "O" : "X"}'s Turn`;
+      board.gameBoard[+box.id - 1] = currentTurn;
+
+      render();
+    });
+  });
+
+  function restart() {
+    board.gameBoard = ["", "", "", "", "", "", "", "", ""];
+    turn.textContent = "X's turn";
+    render();
+  }
+
+  function getBoard() {
+    return board.gameBoard;
+  }
+
+  return { getBoard, restart };
+})();
