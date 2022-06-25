cdda3fcc4023622c927d3c2bdda01d29afa0b7a4
Sharkri <sharkdude955@gmail.com>
2022-06-25 01:18:06 -0500

    players can pick a  name and also added style

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,39 +1,66 @@
 const game = (() => {
-  const board = {
-    gameBoard: ["", "", "", "", "", "", "", "", ""],
-  };
-
   const marker = document.querySelectorAll(".marker");
   const turn = document.querySelector(".turn");
+  let currentTurn = "O";
+  const board = {
+    gameBoard: ["", "", "", "", "", "", "", "", ""],
+
+    eventMarkerPlaced: marker.forEach((box) => {
+      box.addEventListener("click", () => {
+        if (box.textContent) return;
+        changeTurn(box);
+        render();
+      });
+    }),
+  };
+
+  submitName = function () {
+    const inputs = document.querySelectorAll("input");
+
+    if (!(inputs[0].value && inputs[1].value)) return;
+    for (let input of inputs) playerNames.push(input.value);
+    // Default value for turn
+    turn.textContent = `${playerNames[0]}'s turn`;
+
+    const home = document.querySelector(".home");
+    home.style.display = "none";
+    const openGame = document.querySelector(".game");
+    openGame.style.display = "flex";
+  };
+
+  const playerNames = [];
 
   function render() {
     for (let divNum = 0; divNum < board.gameBoard.length; divNum++) {
       marker[divNum].textContent = board.gameBoard[divNum];
     }
+    if (!board.gameBoard.includes("")) console.log("game finish");
   }
 
-  // Event Listener
-  marker.forEach((box) => {
-    box.addEventListener("click", () => {
-      if (box.textContent) return;
+  function changeTurn(box) {
+    if (currentTurn === "X") {
+      currentTurn = "O";
+    } else {
+      currentTurn = "X";
+    }
 
-      currentTurn = turn.textContent.includes("X") ? "X" : "O";
-      turn.textContent = `${currentTurn === "X" ? "O" : "X"}'s Turn`;
-      board.gameBoard[+box.id - 1] = currentTurn;
+    turn.textContent = `${
+      currentTurn === "X" ? playerNames[1] : playerNames[0]
+    }'s turn`;
 
-      render();
-    });
-  });
-
+    board.gameBoard[+box.id - 1] = currentTurn;
+  }
   function restart() {
     board.gameBoard = ["", "", "", "", "", "", "", "", ""];
-    turn.textContent = "X's turn";
+    turn.textContent = `${playerNames[0]}'s turn`; // aple
     render();
   }
 
   function getBoard() {
     return board.gameBoard;
   }
-
-  return { getBoard, restart };
+  function getNames() {
+    return playerNames;
+  }
+  return { getBoard, restart, submitName, getNames };
 })();
