10e34439720507ed8d1c3b3f82b444a76e8b675f
Sharkri <sharkdude955@gmail.com>
2022-07-03 23:10:35 -0500

    added robot modes and made code cleaner

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -2,163 +2,173 @@ function Player(name, type, marker) {
   return { name, type, marker };
 }
 
+const Gameboard = (() => {
+  let board = ["", "", "", "", "", "", "", "", ""];
+
+  const getBoard = () => board;
+
+  const resetBoard = () => (board = ["", "", "", "", "", "", "", "", ""]);
+
+  const changeBoard = (num, marker) => (board[num] = marker);
+
+  return { getBoard, resetBoard, changeBoard };
+})();
+
+const displayController = (() => {
+  const p1 = document.querySelector(".p1");
+  const p2 = document.querySelector(".p2");
+  const getPlayers = () => players;
+  const getPlayerInfo = (num, attr) => players[num][attr];
+  let players = [];
+  function changeOverlay() {
+    const home = document.querySelector(".home");
+    const openGame = document.querySelector(".game");
+    if (home.style.display === "none") home.style.display = "flex";
+    else home.style.display = "none";
+
+    if (openGame.style.display === "flex") openGame.style.display = "none";
+    else openGame.style.display = "flex";
+  }
+
+  function goBack() {
+    changeOverlay();
+    game.resetWins();
+  }
+
+  function startGame() {
+    addPlayerToArray();
+    p1.textContent = getPlayerInfo(0, "name");
+    p2.textContent = getPlayerInfo(1, "name");
+    game.restart();
+    changeOverlay();
+  }
+
+  function addPlayerToArray() {
+    const inputs = document.querySelectorAll("input[type='text']");
+    players = [];
+    let defaults = ["Player One", "Player Two"];
+    for (let i = 0; i < inputs.length; i++) {
+      let name = inputs[i].value ? inputs[i].value : defaults[i];
+      players.push(Player(name, isPlayerOrAI(i), i === 0 ? "X" : "O"));
+    }
+  }
+
+  function isPlayerOrAI(numType) {
+    const P1Radio = document.querySelector("#player1Radio");
+    if (numType === 0) return P1Radio.checked ? "Player" : "AI";
+
+    const P2Radio = document.querySelector("#player2Radio");
+    return P2Radio.checked ? "Player" : "AI";
+  }
+  document.querySelector(".start").addEventListener("click", startGame);
+  document.querySelector(".back").addEventListener("click", goBack);
+
+  return { getPlayers, getPlayerInfo };
+})();
+
 const game = (() => {
   const marker = document.querySelectorAll(".marker");
   const turn = document.querySelector(".turn");
-  const p1 = document.querySelector(".p1");
-  const p2 = document.querySelector(".p2");
   const p1Wins = document.querySelector(".p1-wins");
   const p2Wins = document.querySelector(".p2-wins");
   const p1Mode = document.querySelector("#P1-Modes");
   const p2Mode = document.querySelector("#P2-Modes");
-
-  const board = {
-    gameBoard: ["", "", "", "", "", "", "", "", ""],
-    eventMarkerPlaced: marker.forEach((box) => {
-      box.addEventListener("click", () => {
-        if (box.textContent || checkWinner(board.gameBoard)) return;
-        const objectAI = players[players.findIndex((x) => x.type === "AI")];
-        if (objectAI && objectAI.marker === (lastTurn === "X" ? "O" : "X"))
-          return;
-        if (players[0].type === "AI" && players[1].type === "AI") return;
-        let boxNum = Number(box.id) - 1;
-        makeMove(boxNum);
-        if (objectAI && objectAI.type)
-          AIMove(objectAI.marker === "X" ? p1Mode.value : p2Mode.value);
-      });
-    }),
-  };
+  const board = Gameboard.getBoard;
+  const changeBoard = Gameboard.changeBoard;
+  const players = displayController.getPlayerInfo;
+  marker.forEach((box) => {
+    box.addEventListener("click", () => {
+      const player = displayController.getPlayers();
+      if (box.textContent || checkWinner(board())) return;
+      // Gets AI Object Index
+      const aiObject = player[player.findIndex((x) => x.type === "AI")];
+      if (aiObject && aiObject.marker !== lastTurn) return;
+      if (player[0].type === "AI" && player[1].type === "AI") return;
+      let boxNum = Number(box.id) - 1;
+      makeMove(boxNum);
+      if (aiObject && aiObject.type)
+        AIMove(aiObject.marker === "X" ? p1Mode.value : p2Mode.value);
+    });
+  });
   let lastTurn = "O";
-  let players = [];
 
-  const overlay = {
-    changeOverlay: function () {
-      const home = document.querySelector(".home");
-      const openGame = document.querySelector(".game");
-      if (home.style.display === "none") home.style.display = "flex";
-      else home.style.display = "none";
-
-      if (openGame.style.display === "flex") openGame.style.display = "none";
-      else openGame.style.display = "flex";
-    },
-
-    goBack: function () {
-      overlay.changeOverlay();
-      p2Wins.textContent = p1Wins.textContent = "0";
-    },
-
-    startGame: function () {
-      overlay.addPlayerToArray();
-      p1.textContent = players[0].name;
-      p2.textContent = players[1].name;
-      restart();
-      overlay.changeOverlay();
-    },
-    addPlayerToArray: function () {
-      const inputs = document.querySelectorAll("input[type='text']");
-      players = [];
-      let defaultNames = ["Player One", "Player Two"];
-      for (let i = 0; i < inputs.length; i++) {
-        if (inputs[i].value) {
-          players.push(
-            Player(
-              inputs[i].value,
-              overlay.isPlayerOrAI(i),
-              i === 0 ? "X" : "O"
-            )
-          );
-          continue;
-        }
-        players.push(
-          Player(defaultNames[i], overlay.isPlayerOrAI(i), i === 0 ? "X" : "O")
-        );
+  // Radio Button Event Listener
+  const radios = document.querySelectorAll('input[type="radio"]');
+  radios.forEach((radio) => {
+    radio.addEventListener("click", () => {
+      const MODES = document.querySelectorAll(".modes");
+      const PLAYER_MODE = { player1Radio: 0, player2Radio: 1 };
+      if (radio.id === "ai1" || radio.id === "ai2") {
+        const IDNumber = [+radio.id.slice(-1) - 1];
+        MODES[IDNumber].style.display = "block";
       }
-    },
-
-    isPlayerOrAI: function (numType) {
-      const P1Radio = document.querySelector("#player1Radio");
-      if (numType === 0) {
-        return overlay.checkPlayer(P1Radio.checked);
+      if (radio.id === "player1Radio" || radio.id === "player2Radio") {
+        MODES[PLAYER_MODE[radio.id]].style.display = "none";
       }
-      const P2Radio = document.querySelector("#player2Radio");
-      return overlay.checkPlayer(P2Radio.checked);
-    },
-
-    checkPlayer: function (value) {
-      if (value) return "Player";
-      return "AI";
-    },
-  };
-  // Event Listeners
-  document.querySelector(".start").addEventListener("click", overlay.startGame);
-  document.querySelector(".back").addEventListener("click", overlay.goBack);
-  document.querySelectorAll('input[type="radio"]').forEach((element) => {
-    {
-      element.addEventListener("click", () => {
-        const MODES = document.querySelectorAll(".modes");
-        const PLAYER_MODE = { player1Radio: 0, player2Radio: 1 };
-        if (element.id === "ai1" || element.id === "ai2") {
-          const IDNumber = [+element.id.slice(-1) - 1];
-          MODES[IDNumber].style.display = "block";
-        }
-        if (element.id === "player1Radio" || element.id === "player2Radio") {
-          MODES[PLAYER_MODE[element.id]].style.display = "none";
-        }
-      });
-    }
+    });
   });
 
   function render() {
-    for (let divNum = 0; divNum < board.gameBoard.length; divNum++) {
-      marker[divNum].textContent = board.gameBoard[divNum];
+    for (let divNum = 0; divNum < board().length; divNum++) {
+      marker[divNum].textContent = board()[divNum];
     }
     highlightTurn();
-    if (checkWinner(board.gameBoard) === "Draw") {
+    if (checkWinner(board()) === "Draw") {
       turn.textContent = "Draw!";
       return;
     }
     const markers = { X: "X", O: "O" };
-    if (checkWinner(board.gameBoard) in markers) {
+    if (checkWinner(board()) in markers) {
       if (lastTurn === "X") {
-        turn.textContent = `${players[0].name} is the winner!`;
+        turn.textContent = `${players(0, "name")} is the winner!`;
         p1Wins.textContent = `${(+p1Wins.textContent + 1).toString()}`;
         return;
       }
 
-      turn.textContent = `${players[1].name} is the winner!`;
+      turn.textContent = `${players(1, "name")} is the winner!`;
       p2Wins.textContent = `${(+p2Wins.textContent + 1).toString()}`;
     }
   }
+  const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
 
   async function AIMove(mode) {
     let boxToPlaceMarker;
-    if (checkWinner(board.gameBoard)) return;
+    if (checkWinner(board())) return;
 
     if (mode === "Easy") boxToPlaceMarker = easyMode();
-    if (mode === "Medium") boxToPlaceMarker = mediumMode(board.gameBoard);
-    if (mode === "Impossible") boxToPlaceMarker = impossibleMode();
+    else if (mode === "Medium") boxToPlaceMarker = mediumMode(board());
+    else boxToPlaceMarker = impossibleMode();
 
     await sleep(500);
     makeMove(boxToPlaceMarker);
-    if (players[0].type === "AI" && players[1].type === "AI") {
+    if (players(0, "type") === "AI" && players(1, "type") === "AI") {
       AIMove(lastTurn === "X" ? p2Mode.value : p1Mode.value);
     }
   }
+
+  const impossibleMode = () => getBestMove(board());
+
   function mediumMode(board) {
     const currentTurn = lastTurn === "X" ? "O" : "X";
     let move = aboutToWin(board, currentTurn) || aboutToWin(board, lastTurn);
     return move !== undefined ? move : easyMode();
   }
-  const impossibleMode = () => getBestMove(board.gameBoard);
+
+  function easyMode() {
+    randomMove = Math.floor(Math.random() * 9);
+    while (board()[randomMove]) {
+      randomMove = Math.floor(Math.random() * 9);
+    }
+    return randomMove;
+  }
 
   function getBestMove(board) {
     const currentTurn = lastTurn === "X" ? "O" : "X";
     const turnsPlayed = board.filter((a) => a == lastTurn).length;
-    console.log(turnsPlayed);
-    let bestMove;
     const diag = (board[0] && board[8]) || (board[2] && board[6]);
     const corners = board[0] || board[8] || board[2] || board[6];
     const midCorner = board[1] || board[5] || board[7] || board[3];
+    let bestMove;
     bestMove = aboutToWin(board, currentTurn) || aboutToWin(board, lastTurn);
     if (turnsPlayed == 0) bestMove = 0;
     if (turnsPlayed == 1 && currentTurn === "X" && board[4]) bestMove = 8;
@@ -185,16 +195,8 @@ const game = (() => {
       if (isWinner) return i;
     }
   }
-  function easyMode() {
-    randomMove = Math.floor(Math.random() * 9);
-    while (board.gameBoard[randomMove]) {
-      randomMove = Math.floor(Math.random() * 9);
-    }
-    return randomMove;
-  }
 
   const checkGameOver = (board) => !board.includes("");
-  const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
 
   function makeMove(boxNum) {
     changeTurn(boxNum);
@@ -204,12 +206,12 @@ const game = (() => {
   function changeTurn(boxNum) {
     lastTurn = lastTurn === "X" ? "O" : "X";
     displayTurn();
-    board.gameBoard[boxNum] = lastTurn;
+    changeBoard(boxNum, lastTurn);
   }
 
   const displayTurn = () => {
     turn.textContent = `${
-      lastTurn === "O" ? players[0].name : players[1].name
+      lastTurn === "O" ? players(0, "name") : players(1, "name")
     }'s turn`;
     highlightTurn();
   };
@@ -221,25 +223,23 @@ const game = (() => {
     first.style.backgroundColor = second.style.backgroundColor = "#f7f7f7";
     first.style.boxShadow = second.style.boxShadow = "none";
 
-    if (checkWinner(board.gameBoard)) return;
+    if (checkWinner(board())) return;
 
-    if (lastTurn === "O") {
-      first.style.backgroundColor = "#fca5a5";
-      first.style.boxShadow = "rgba(0, 0, 0, 0.25) 0px 25px 50px -12px";
-      return;
-    }
-
-    second.style.backgroundColor = "#fca5a5";
-    second.style.boxShadow = "rgba(0, 0, 0, 0.25) 0px 25px 50px -12px";
+    lastTurn === "O" ? turnStyle(first) : turnStyle(second);
   }
 
   function restart() {
-    board.gameBoard = ["", "", "", "", "", "", "", "", ""];
-    turn.textContent = `${players[0].name}'s turn`;
+    Gameboard.resetBoard();
+    turn.textContent = `${players(0, "name")}'s turn`;
     lastTurn = "O";
 
-    if (players[0].type === "AI") AIMove(p1Mode.value);
+    if (players(0, "type") === "AI") AIMove(p1Mode.value);
     render();
+  }
+
+  function turnStyle(num) {
+    num.style.backgroundColor = "#fca5a5";
+    num.style.boxShadow = "rgba(0, 0, 0, 0.25) 0px 25px 50px -12px";
   }
 
   function checkWinner(board) {
@@ -266,13 +266,10 @@ const game = (() => {
     for (let item of array) {
       if (item.includes("")) continue;
       const sorted = item.sort();
-      const onlyContains = sorted[0] == sorted[sorted.length - 1];
+      const onlyContains = sorted[0] == sorted.slice(-1);
       if (onlyContains) return item[0];
     }
   }
-  const getPlayersInfo = () => {
-    for (let player of players) console.log(player);
-  };
-  const getBoard = () => board.gameBoard;
-  return { restart, checkWinner, getPlayersInfo, getBoard };
+  const resetWins = () => (p1Wins.textContent = p2Wins.textContent = "0");
+  return { restart, checkWinner, resetWins };
 })();
